2ae8b9f616c021bb21bcb34c9f5198b29d2daeed
Frederick Price <rprice@pricemail.ca>
2022-12-25 03:27:20 -0500

    Adding some database commands

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,20 @@
 module Main where
 
 import HaskellSay (haskellSay)
+import Database.HDBC
+import Database.HDBC.Sqlite3
 
 main :: IO ()
 main =
-  haskellSay "Hello, Haskell! You're using a function from another package!"
+    do
+        putStrLn "About to open sqlite database test1.db"
+        conn <- connectSqlite3 "test1.db"
+        putStrLn "Creating a table"
+        run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
+        putStrLn "Inserting data"
+        run conn "INSERT INTO test (id) VALUES (0)" []
+        putStrLn "Commiting"
+        commit conn
+        putStrLn "Disconnecting"
+        disconnect conn
+
