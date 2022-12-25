6dcebe0b84a9fd69edd619d7267fb8ed050ab429
Rick Price <rprice@pricemail.ca>
2022-12-25 01:35:14 +0000

    Initial commit

---
+++ dist-newstyle/build/x86_64-linux/ghc-8.10.6/HaskellLearningCabal-0.1.0.0/x/HaskellLearningCabal/build/HaskellLearningCabal/autogen/Paths_HaskellLearningCabal.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -Wno-missing-safe-haskell-mode #-}
+module Paths_HaskellLearningCabal (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/runner/HaskellLearningCabal/.config/cabal/bin"
+libdir     = "/home/runner/HaskellLearningCabal/.config/cabal/lib/x86_64-linux-ghc-8.10.6/HaskellLearningCabal-0.1.0.0-inplace-HaskellLearningCabal"
+dynlibdir  = "/home/runner/HaskellLearningCabal/.config/cabal/lib/x86_64-linux-ghc-8.10.6"
+datadir    = "/home/runner/HaskellLearningCabal/.config/cabal/share/x86_64-linux-ghc-8.10.6/HaskellLearningCabal-0.1.0.0"
+libexecdir = "/home/runner/HaskellLearningCabal/.config/cabal/libexec/x86_64-linux-ghc-8.10.6/HaskellLearningCabal-0.1.0.0"
+sysconfdir = "/home/runner/HaskellLearningCabal/.config/cabal/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "HaskellLearningCabal_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "HaskellLearningCabal_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "HaskellLearningCabal_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "HaskellLearningCabal_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "HaskellLearningCabal_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "HaskellLearningCabal_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
