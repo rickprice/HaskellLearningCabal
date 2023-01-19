3d4184b9e7293285d8b8cb87581fa831a9dedae3
Frederick Price <rprice@pricemail.ca>
2023-01-19 17:41:30 -0500

    Got rid of all the warnings

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
-module Main where
+{-# LANGUAGE Unsafe #-}
+module Main (main) where
 
--- import HaskellSay (haskellSay)
+import Prelude
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
-add a b = a + b
+-- add :: Num a => a -> a -> a
+-- add a b = a + b
 
 main :: IO ()
 main =
@@ -12,9 +14,9 @@ main =
         putStrLn "About to open sqlite database test1.db"
         conn <- connectSqlite3 "test1.db"
         putStrLn "Creating a table"
-        run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
+        _ <- run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
         putStrLn "Inserting data"
-        run conn "INSERT INTO test (id) VALUES (0)" []
+        _ <- run conn "INSERT INTO test (id) VALUES (0)" []
         putStrLn "Commiting"
         commit conn
         putStrLn "Disconnecting"
