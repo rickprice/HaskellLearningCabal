b5e25de322ac1c2c22a316ce6bffd1dd214f7766
Frederick Price <rprice@pricemail.ca>
2023-01-19 18:03:33 -0500

    Use placement variables, formatted with "fourmolu -m inplace  app/Main.hs"

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE Unsafe #-}
+
 module Main (main) where
 
-import Prelude
 import Database.HDBC
 import Database.HDBC.Sqlite3
+import Prelude
 
 -- add :: Num a => a -> a -> a
 -- add a b = a + b
@@ -16,9 +17,8 @@ main =
         putStrLn "Creating a table"
         _ <- run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
         putStrLn "Inserting data"
-        _ <- run conn "INSERT INTO test (id) VALUES (0)" []
+        _ <- run conn "INSERT INTO test (id) VALUES (?)" [toSql (1 :: Int)]
         putStrLn "Commiting"
         commit conn
         putStrLn "Disconnecting"
         disconnect conn
-
