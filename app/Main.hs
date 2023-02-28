3d557134928347de100e1a83ba962daddfdedfee
Frederick Price <rprice@pricemail.ca>
2023-02-28 13:18:36 -0500

    Just trying to test out the hls

--- app/Main.hs
+++ app/Main.hs
@@ -11,14 +11,14 @@ import Prelude
 
 main :: IO ()
 main =
-    do
-        putStrLn "About to open sqlite database test1.db"
-        conn <- connectSqlite3 "test1.db"
-        putStrLn "Creating a table"
-        _ <- run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
-        putStrLn "Inserting data"
-        _ <- run conn "INSERT INTO test (id) VALUES (?)" [toSql (1 :: Int)]
-        putStrLn "Commiting"
-        commit conn
-        putStrLn "Disconnecting"
-        disconnect conn
+  do
+    putStrLn "About to open sqlite database test1.db"
+    conn <- connectSqlite3 "test1.db"
+    putStrLn "Creating a table"
+    _ <- run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
+    putStrLn "Inserting data"
+    _ <- run conn "INSERT INTO test (id) VALUES (?)" [toSql (1 :: Int)]
+    putStrLn "Commiting"
+    commit conn
+    putStrLn "Disconnecting"
+    disconnect conn
